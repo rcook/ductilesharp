444b3cf09787f1712169bd3f118616f9a468abb4
Richard Cook <rcook@rprodev.com>
2010-10-23 12:48:39 -0700

    Restructured build environment to support building against target framework and with add-ins
    * Updated COMPILING document
    * Added "help" target to Ant build listing all available (top-level) targets
    * Renamed "test" target to "unit" for running unit tests
    * Implemented "install" and associated targets for deploying add-in to framework directory etc.
    * Rejigged "common.properties" a little
    * Rewrote "configure" to implement these changes
    * Implemented "IAddIn" interface and added reference to "dmcs" to "Mono.DuctileSharp.dll" project
    * Created "hello world" test program

--- src/dll/AddIn.cs
+++ src/dll/AddIn.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
+using Mono.CompilerServices.Extensibility;
 
 namespace Mono.DuctileSharp
 {
-    public sealed class AddIn
+    public sealed class AddIn : IAddIn
     {
+        private bool _detype = false;
+
+        #region IAddIn Members
+
+        string IAddIn.Name {get {return "DuctileSharp";}}
+
+        string IAddIn.Description {get {return "Detype your C# code!";}}
+
+        bool IAddIn.ParseCommandLineOption(string arg)
+        {
+            if ("/detype".Equals(arg, StringComparison.InvariantCultureIgnoreCase))
+            {
+                _detype = true;
+                return true;
+            }
+            return false;
+        }
+
+        void IAddIn.ApplyTypeTransform(ITypeExpressionFactory typeExprFactory, IEnumerable<ITypeInfo> types)
+        {
+        }
+
+        #endregion IAddIn Members
+
         public AddIn()
         {
             Console.WriteLine("AddIn..ctor");
@@ -11,7 +37,7 @@ namespace Mono.DuctileSharp
 
         public void DoSomething()
         {
-            Console.WriteLine("AddIn.DoSomething");
+            Console.WriteLine("AddIn.DoSomething: detype = {0}", _detype.ToString().ToLowerInvariant());
         }
     }
 }
---
+++ test/01-hello-world/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
+
